20fa25f9e29848d8504894a78f850f43546cfc28
Denis Fomin <82903704+gmmmmd@users.noreply.github.com>
2022-09-21 12:56:47 +0300

    Add header

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,23 @@
 window.addEventListener('DOMContentLoaded', function () {
   const body = document.querySelector('body');
-  
+
   (function toggleMobileMenu() {
     const buttonBurgerMenu = document.querySelector('.js-header__burger');
 
     if (buttonBurgerMenu) {
       buttonBurgerMenu.addEventListener('click', function() {
         body.classList.toggle('mobile-menu--open');
+      });
+    };
+  })();
+
+  (function toggleLocationCity() {
+    const buttonLocationMenu = document.querySelector('.js-location__btn');
+    const containerLocationMenu = document.querySelector('.js-location__search-container');
+
+    if (buttonLocationMenu) {
+      buttonLocationMenu.addEventListener('click', function() {
+        containerLocationMenu.classList.toggle('location__search-container-open');
       });
     };
   })();
